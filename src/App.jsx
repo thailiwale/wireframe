61521dbd65681260f8c9be8c95da614a27455e30
Harshit Gupta <gharshit521@gmail.com>
2025-07-14 17:07:33 +0530

    Add working route config with /flexo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Flexo from './pages/Flexo';
-import Dashboard from './pages/Dashboard'; // Optional, add only if created
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public Route */}
+        {/* Login page */}
         <Route path="/" element={<Login />} />
 
-        {/* Protected Route: Flexo */}
+        {/* Flexo page (protected) */}
         <Route
           path="/flexo"
           element={
             <PrivateRoute page="Flexo">
               <Flexo />
-            </PrivateRoute>
-          }
-        />
-
-        {/* Optional: Protected Dashboard Route */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute page="Dashboard">
-              <Dashboard />
             </PrivateRoute>
           }
         />
